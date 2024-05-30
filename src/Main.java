dbd679155ca9f9d87eafc432fd7936e921abfab1
Andrey Trofimov <andreytrofimov642@gmail.com>
2024-05-30 14:31:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        int c = 2;
+        int d = 3;
+            System.out.println(a + "   " + b + "   " + c + "   " + d);
+        }
+    }
